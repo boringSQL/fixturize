487dc7584e431f6ebde92f74ecd16869d362df28
Radim Marek <radim@boringsql.com>
2026-02-23 23:26:37 +0100

    fix: \r missed

--- fixturize/apply.go
+++ fixturize/apply.go
@@ -242,6 +242,7 @@ func escapeCopyText(s string) string {
 	s = strings.ReplaceAll(s, `\`, `\\`)
 	s = strings.ReplaceAll(s, "\t", `\t`)
 	s = strings.ReplaceAll(s, "\n", `\n`)
+	s = strings.ReplaceAll(s, "\r", `\r`)
 	return s
 }
 
