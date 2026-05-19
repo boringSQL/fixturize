98abba3c3f9cb290ec52ac0a75158c9100a801c1
Radim Marek <radim@boringsql.com>
2026-05-19 21:52:37 +0200

    chore: extract shared masks schema into masking package
    For https://github.com/boringSQL/dryrun/issues/15

---
+++ masking/doc.go
@@ -0,0 +1,4 @@
+// Package masking holds the data-masking-policy.yml schema and the
+// loader/discovery used by fixturize. Stdlib + yaml only, so other
+// tools can read the same file without pulling the rest of fixturize.
+package masking
